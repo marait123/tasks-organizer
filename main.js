116a3325d32c7391cfa1de385b7a9571a5670164
marait123 <max.ibrahim43@hotmail.com>
2021-02-19 02:34:01 +0200

    added  the todos list

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const electron = require("electron");
 // const app = electron.app;
 // const BrowserWindow = electron.BrowserWindow;
 //const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
-const fs = require('fs');
+const path = require("path");
+const url = require("url");
+const fs = require("fs");
 const { dir } = require("console");
 // let shell = electron.shell;
-// const dialog  = electron.dialog; 
-const {shell,dialog, BrowserWindow,app, Menu, ipcMain} = electron;
+// const dialog  = electron.dialog;
+const { shell, dialog, BrowserWindow, app, Menu, ipcMain } = electron;
 
 // shell.openExternal(__dirname)
 
@@ -33,109 +33,81 @@ const directoryPath = __dirname;
 //     //handling error
 //     if (err) {
 //         return console.log('Unable to scan directory: ' + err);
-//     } 
+//     }
 //     //listing all files using forEach
 //     files.forEach(function (file) {
 //         // Do whatever you want to do with the file
-//         console.log(file); 
+//         console.log(file);
 //     });
 // });
 
 let win;
 
-function createWindow () {
+function createWindow() {
   // console.log(dialog.showOpenDialog({ properties: ['openDirectory', 'multiSelections'] }))
   win = new BrowserWindow({
-    width: 800, 
+    width: 800,
     height: 600,
     webPreferences: {
-        nodeIntegration: true
-    }})
-
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
-  // win.webContents.openDevTools();
-  win.on('closed', () => {
-    win = null
+      nodeIntegration: true,
+    },
   });
-  
 
-  const isMac = process.platform === 'darwin'
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+  // win.webContents.openDevTools();
+  win.on("closed", () => {
+    win = null;
+  });
+
+  const isMac = process.platform === "darwin";
 
   const template = [
     {
-      label: 'File',
-      submenu: [
-        isMac ? { role: 'close' } : { role: 'quit' }
-      ]
+      label: "File",
+      submenu: [isMac ? { role: "close" } : { role: "quit" }],
     },
     // { role: 'windowMenu' }
     {
-      label: 'Window',
-      submenu: [
-        { role: 'minimize' },
-        { role: 'zoom' },
-          { role: 'close' }        
-      ]
-    }    
-  ]
-  if(process.env.NODE_ENV !== 'production'){
-    template.push(
+      label: "Window",
+      submenu: [{ role: "minimize" }, { role: "zoom" }, { role: "close" }],
+    },
+  ];
+  if (process.env.NODE_ENV !== "production") {
+    template.push({
+      label: "view",
 
-      {
-        label:"view",
-  
-        submenu:[{ role: 'toggleDevTools',accelerator:'f12' }],
-        
-  
-      }
-    )
+      submenu: [{ role: "toggleDevTools", accelerator: "f12" }],
+    });
   }
-  const menu = Menu.buildFromTemplate(template)
-  Menu.setApplicationMenu(menu)
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 }
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (win === null) {
     createWindow();
   }
 });
 
-
-// SECTION: ipc
-
-ipcMain.on('2', async (event, path) => { 
-  console.log("show_dialog received");
-  var dirs = await dialog.showOpenDialog({ properties: ['openDirectory', 'multiSelections'] });
-  console.log(dirs)
-  if(dirs == undefined){
-    event.sender.sendSync('dirs', {dirs: "dirs"})
-  }else{
-    event.sender.sendSync('dirs', {
-      dirs:dirs
-    });
-  }
-
-});
-
-
 // Event handler for asynchronous incoming messages
-ipcMain.on('show_dialog', async (event, arg) => {
-  console.log(arg)
-  var dirs = await dialog.showOpenDialog({ properties: ['openDirectory', 'multiSelections'] });
+ipcMain.on("show_dialog", async (event, arg) => {
+  console.log(arg);
+  var dirs = await dialog.showOpenDialog({ properties: ["openDirectory"] });
   console.log("dirs");
-  console.log(dirs);
+
   // Event emitter for sending asynchronous messages
-  event.sender.send('show_dialog-reply', dirs)
-})
+  event.sender.send("dir", dirs);
+});
--- views.js
+++ views.js
@@ -1,29 +1,51 @@
- // const ipc = electron.ipcRenderer
+// const ipc = electron.ipcRenderer
+const fs = require("fs");
+const $ = require("jquery");
+const { ipcRenderer } = require("electron");
 
- const { ipcRenderer } = require('electron')
- function readFile(){
-// Synchronous message emmiter and handler
+function generate_todos(files) {
+    $("#welcome-div").hide();
+    $("#main-div").append(`<div id="todo-div"></div>`);
+    console.log("files");
+    let i = 0;
+    files.forEach((value, index, arr) => {
+        console.log(value);
+        $("#todo-div")
+            .append(`<div class="todo-item">  <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+        <label for="vehicle1"> I have a bike</label>${i}. ${value}</div>`);
+    });
 
-    // Async message handler
-    ipcRenderer.on('show_dialog-reply', (event, arg) => {
-    console.log(arg);
-    if(arg.canceled == false && arg.filePaths.length > 0){
-        console.log("render todos");
-    }
-    })
-
-    // Async message sender
-    ipcRenderer.send('show_dialog', 'show_dialog')
-
+    // console.log(files);
 }
 
+const show_dialog = () => {
+    // test
+    // generate_todos(["hi 1", "hi 2", "hi 3"]);
+    // return;
+    ipcRenderer.sendSync("show_dialog", {
+        do: "show_dialog",
+    });
 
- const show_dialog = () => {
-    ipcRenderer.sendSync('show_dialog', {
-        do:"show_dialog"
-    }) 
-     
-     ipcRenderer.on('dirs', (event, data) => { 
+    ipcRenderer.on("dir", (event, data) => {
+        console.log("recieved: ");
         console.log(data);
-     }) 
-    }
+        let files = fs.readdirSync(data.filePaths[0]);
+        generate_todos(files);
+    });
+};
+
+// TODO: remove
+function readFile() {
+    // Synchronous message emmiter and handler
+
+    // Async message handler
+    ipcRenderer.on("show_dialog-reply", (event, arg) => {
+        console.log(arg);
+        if (arg.canceled == false && arg.filePaths.length > 0) {
+            console.log("render todos");
+        }
+    });
+
+    // Async message sender
+    ipcRenderer.send("show_dialog", "show_dialog");
+}
