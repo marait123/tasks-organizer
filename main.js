9be6f75abaad7c21718f136d59f375b69617cfa2
marait123 <max.ibrahim43@hotmail.com>
2021-03-19 10:02:58 +0200

    file open works fine

--- main.js
+++ main.js
@@ -44,109 +44,129 @@ const directoryPath = __dirname;
 let win;
 
 async function createWindow() {
-    // console.log(dialog.showOpenDialog({ properties: ['openDirectory', 'multiSelections'] }))
+	// console.log(dialog.showOpenDialog({ properties: ['openDirectory', 'multiSelections'] }))
 
-    win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
+	win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
 
-    win.loadURL(
-        url.format({
-            pathname: path.join(__dirname, "index.html"),
-            protocol: "file:",
-            slashes: true,
-        })
-    );
-    // win.webContents.openDevTools();
-    win.on("closed", () => {
-        win = null;
-    });
+	win.loadURL(
+		url.format({
+			pathname: path.join(__dirname, "index.html"),
+			protocol: "file:",
+			slashes: true,
+		})
+	);
+	// win.webContents.openDevTools();
+	win.on("closed", () => {
+		win = null;
+	});
 
-    const isMac = process.platform === "darwin";
+	const isMac = process.platform === "darwin";
 
-    const template = [
-        {
-            label: "File",
-            submenu: [
-                isMac
-                    ? {
-                          role: "close",
-                      }
-                    : {
-                          role: "quit",
-                      },
-                {
-                    label: "close",
-                    click: async () => {
-                        console.log("clicked");
-                    },
-                },
-            ],
-        },
-        // { role: 'windowMenu' }
-        {
-            label: "Window",
-            submenu: [
-                { role: "minimize" },
-                { role: "zoom" },
-                { role: "close" },
-            ],
-        },
-    ];
-    if (process.env.NODE_ENV !== "production") {
-        template.push({
-            label: "view",
-            submenu: [{ role: "toggleDevTools", accelerator: "f12" }],
-        });
-    }
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
+	const template = [
+		{
+			label: "File",
+			submenu: [
+				isMac
+					? {
+							role: "close",
+					  }
+					: {
+							role: "quit",
+					  },
+				{
+					label: "close",
+					click: async () => {
+						console.log("clicked");
+					},
+				},
+			],
+		},
+		// { role: 'windowMenu' }
+		{
+			label: "Window",
+			submenu: [
+				{ role: "minimize" },
+				{ role: "zoom" },
+				{ role: "close" },
+			],
+		},
+	];
+	if (process.env.NODE_ENV !== "production") {
+		template.push({
+			label: "view",
+			submenu: [{ role: "toggleDevTools", accelerator: "f12" }],
+		});
+	}
+	const menu = Menu.buildFromTemplate(template);
+	Menu.setApplicationMenu(menu);
 }
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
 app.on("activate", () => {
-    if (win === null) {
-        createWindow();
-    }
+	if (win === null) {
+		createWindow();
+	}
 });
 
 // Event handler for asynchronous incoming messages
 ipcMain.on("show_dialog", async (event, arg) => {
-    console.log(arg);
-    var dir_obj = await dialog.showOpenDialog({
-        properties: ["openDirectory", "openFile"],
-    });
-    console.log("dir_obj");
+	console.log(arg);
+	var dir_obj = await dialog.showOpenDialog({
+		properties: ["openDirectory", "openFile"],
+	});
+	console.log("dir_obj");
 
-    // Event emitter for sending asynchronous messages
-    event.sender.send("dir", dir_obj);
+	// Event emitter for sending asynchronous messages
+	event.sender.send("dir", dir_obj);
 });
 
 // Event handler for asynchronous incoming messages
 ipcMain.on("save_dialog", async (event, arg) => {
-    console.log("arguments ent to save dialog");
-    console.log(arg);
-    var file_obj = await dialog.showSaveDialog({
-        title: "todo.json",
-        filters: [
-            {
-                name: "todo",
-                extensions: ["json", "todo"],
-            },
-        ],
-    });
-    console.log("saved_file");
-    console.log(file_obj);
-    // Event emitter for sending asynchronous messages
-    event.sender.send("saved_file", file_obj);
+	console.log("arguments ent to save dialog");
+	console.log(arg);
+	var file_obj = await dialog.showSaveDialog({
+		title: "todo.json",
+		filters: [
+			{
+				name: "todo",
+				extensions: ["json", "todo"],
+			},
+		],
+	});
+	console.log("saved_file");
+	console.log(file_obj);
+	// Event emitter for sending asynchronous messages
+	event.sender.send("saved_file", file_obj);
+});
+// these handlers can be done using handlers
+// Event handler for asynchronous incoming messages
+ipcMain.on("load_dialog", async (event, arg) => {
+	console.log("arguments ent to save dialog");
+	console.log(arg);
+	var file_obj = await dialog.showOpenDialog({
+		title: "todo.json",
+		properties: ["openFile"],
+		filters: [
+			{
+				name: "todo",
+				extensions: ["json", "todo"],
+			},
+		],
+	});
+	console.log("loaded_file");
+	console.log(file_obj);
+	// Event emitter for sending asynchronous messages
+	event.sender.send("loaded_file", file_obj);
 });
--- views.js
+++ views.js
@@ -18,49 +18,153 @@ const { ipcRenderer } = require("electron");
 }
 */
 app_state = {
-    title: "untitled.",
-    completed: false,
-    saved: false,
-    full_path: ".",
-    name: "untitled.todo",
-    text: "None",
-    subTodos: [],
+	title: "untitled",
+	completed: false,
+	saved: false,
+	modified: false,
+	filePath: ".",
+	name: "untitled.todo",
+	text: "None",
+	subTodos: [],
+	check_todo(index, checked) {
+		this.modified = true;
+		this.subTodos[index].completed = checked;
+	},
+	save(filePath) {
+		if (!filePath) {
+			filePath = this.filePath;
+		}
+		this.filePath = filePath;
+		this.saved = true;
+		this.modified = false;
+		console.log("appstate");
+		console.log(this);
+		let f_string = JSON.stringify(this);
+		console.log("f_string");
+		console.log(f_string);
+		fs.writeFileSync(filePath, f_string);
+	},
+	insertTodo(todoMain, name, text = "", completed = false) {
+		this.modified = true;
+		todo = {
+			name,
+			text: text,
+			completed: completed,
+			subTodos: [],
+		};
+		todoMain.subTodos.push(todo);
+	},
+	load(file_path) {
+		console.log("load is called with ", file_path);
+		let data = fs.readFileSync(file_path, "utf8");
+		let state = JSON.parse(data);
+		console.log("state loaded", state);
+		for (let prop in state) {
+			this[prop] = state[prop];
+		}
+	},
+	render_todos() {
+		// return;
+
+		$("#main-div").append(`<div id="todo-div"></div>`);
+		$("#todo-div").append(`<div class="todo-title">
+				<input
+					type="text"
+					name="todo-title"
+					id="todo-title-input"
+					value="${app_state.title}"
+			/>
+			<input
+				type="button"
+				id="todo-save"
+				value="save"
+				onclick="save_todos()"
+			/>
+		</div>`);
+		let i = 1;
+		var elements = $();
+		this.subTodos.forEach((value, index, arr) => {
+			// console.log(value);
+			// this.insertTodo(app_state, value);
+			elements = elements.add(`<div class="todo-item">  <input type="checkbox" onchange="check_todo_change(${i})" id="${i}" name="todo-${i}" value="done" ${
+				value.completed ? "checked" : ""
+			}>
+        ${i}. ${value.name}</div>`);
+
+			i++;
+		});
+		$("#todo-div").append(elements);
+	},
 };
-function insertTodo(todoMain, name, text = "") {
-    todo = {
-        name,
-        text: text,
-        completed: false,
-        subTodos: [],
-    };
-    todoMain.subTodos.push(todo);
-}
+
 class Screen {
-    constructor() {}
-    render() {}
-    remove() {}
+	constructor() {}
+	render() {}
+	remove() {}
 }
 
 class StartScreen extends Screen {
-    render() {
-        console.log("laoded");
-        $("#main-div").append(`<div id="welcome-div" class="btn-collection">
+	render() {
+		console.log("laoded");
+		$("#main-div").append(`<div id="welcome-div" class="btn-collection">
             <button class="btn-block choose_folder" onclick="show_dialog()">choose folders</button>
             <button class="btn-block open_file" onclick="readFile()">open .todo</button>
           </div> `);
-    }
-    remove() {
-        $("#welcome-div").remove();
-    }
+	}
+	remove() {
+		$("#welcome-div").remove();
+	}
 }
+
+class TodoScreen2 {
+	constructor(subTodos) {
+		this.subTodos = subTodos;
+	}
+	render() {
+		$("#main-div").append(`<div id="todo-div"></div>`);
+		$("#todo-div").append(`<div class="todo-title">
+				<input
+					type="text"
+					name="todo-title"
+					id="todo-title-input"
+					value="${app_state.title}"
+			/>
+			<input
+				type="button"
+				id="todo-save"
+				value="save"
+				onclick="save_todos()"
+			/>
+		</div>`);
+		let i = 1;
+		var elements = $();
+		this.subTodos.forEach((value, index, arr) => {
+			// console.log(value);
+			// this.insertTodo(app_state, value);
+			elements = elements.add(`<div class="todo-item">  <input type="checkbox" onchange="check_todo_change(${i})" id="${i}" name="todo-${i}" value="done" ${
+				value.completed ? "checked" : ""
+			}>
+        ${i}. ${value.name}</div>`);
+
+			i++;
+		});
+		$("#todo-div").append(elements);
+	}
+	remove() {
+		$("#todo-div").remove();
+	}
+}
+
 class TodoScreen {
-    constructor(files) {
-        this.files = files;
-    }
-    render() {}
-    remove() {
-        $("#todo-div").remove();
-    }
+	constructor(files) {
+		this.files = files;
+	}
+	render() {
+		generate_todos(this.files);
+	}
+	remove() {
+		$("#todo-div").remove();
+	}
 }
 
 // screens area
@@ -68,82 +172,97 @@ let start_screen = new StartScreen();
 let current_screen = start_screen;
 
 function render_screen(screen) {
-    current_screen.remove();
-    current_screen = screen;
-    current_screen.render();
+	current_screen.remove();
+	current_screen = screen;
+	current_screen.render();
 }
 
-document.addEventListener("DOMContentLoaded", start_screen.render());
+document.addEventListener("DOMContentLoaded", render_screen(new StartScreen()));
 
+function check_todo_change(id) {
+	let checkbox = document.getElementById(`${id}`);
+	app_state.check_todo(id - 1, checkbox.checked);
+}
 function generate_todos(files) {
-    start_screen.remove();
-    $("#main-div").append(`<div id="todo-div"></div>`);
-    $("#todo-div").append(`<div class="todo-title">
-            <input
-                type="text"
-                name="todo-title"
-                id="todo-title-input"
-                value="untitled"
-        />
-        <input
-            type="button"
-            id="todo-save"
-            value="save"
-            onclick="save_todos()"
-        />
-    </div>`);
-    // console.log("files");
-    let i = 1;
-    var elements = $();
+	// start_screen.remove();
 
-    files.forEach((value, index, arr) => {
-        console.log(value);
-        insertTodo(app_state, value);
+	// console.log("files");
 
-        elements = elements.add(`<div class="todo-item">  <input type="checkbox" id="${i}" name="todo-${i}" value="done">
+	/// temp start
+	files.forEach((value, index, arr) => {
+		app_state.insertTodo(app_state, value);
+	});
+	app_state.render_todos();
+	/// temp end
+	return;
+	let i = 1;
+	var elements = $();
+	files.forEach((value, index, arr) => {
+		console.log(value);
+		app_state.insertTodo(app_state, value);
+
+		elements = elements.add(`<div class="todo-item">  <input type="checkbox" onchange="check_todo_change(${i})" id="${i}" name="todo-${i}" value="done">
         ${i}. ${value}</div>`);
-        i++;
-    });
-    $("#todo-div").append(elements);
+		i++;
+	});
+	$("#todo-div").append(elements);
 }
 
 const show_dialog = () => {
-    // test
-    // generate_todos(["hi 1", "hi 2", "hi 3"]);
-    // return;
-    ipcRenderer.send("show_dialog", {
-        do: "show_dialog",
-    });
+	// test
+	// generate_todos(["hi 1", "hi 2", "hi 3"]);
+	// return;
+	ipcRenderer.send("show_dialog", {
+		do: "show_dialog",
+	});
 
-    ipcRenderer.on("dir", (event, data) => {
-        console.log("recieved: ");
-        console.log(data);
-        let files = fs.readdirSync(data.filePaths[0]);
-        // console.log(files);
-        generate_todos(files);
-    });
+	ipcRenderer.on("dir", (event, data) => {
+		console.log("recieved: ");
+		console.log(data);
+		if (!data.canceled) {
+			let files = fs.readdirSync(data.filePaths[0]);
+			// console.log(files);
+			files.forEach((value, index, arr) => {
+				app_state.insertTodo(app_state, value);
+			});
+			render_screen(new TodoScreen2(app_state.subTodos));
+		}
+	});
 };
 // TODO: save file
 function save_todos() {
-    // ipcRenderer.sendSync('synchronous-message', 'sync ping')
-    // test
-    // generate_todos(["hi 1", "hi 2", "hi 3"]);
-    // return;
-    ipcRenderer.send("save_dialog", {
-        do: "save_dialog",
-    });
+	// ipcRenderer.sendSync('synchronous-message', 'sync ping')
+	// test
+	// generate_todos(["hi 1", "hi 2", "hi 3"]);
+	// return;
+	if (app_state.saved) {
+		app_state.save();
+	} else {
+		ipcRenderer.send("save_dialog", {
+			do: "save_dialog",
+		});
 
-    ipcRenderer.on("saved_file", (event, data) => {
-        console.log("recieved: ");
-        console.log(data);
-        console.log("appstate");
-        console.log(app_state);
-        let f_string = JSON.stringify(app_state);
-        console.log("f_string");
-        console.log(f_string);
-        fs.writeFileSync(data.filePath, f_string);
-    });
+		ipcRenderer.on("saved_file", (event, data) => {
+			console.log("recieved: ");
+			console.log(data);
+			if (!data.canceled) {
+				app_state.save(data.filePath);
+			}
+		});
+	}
 }
 
-// TODO: remove
-function readFile() {}
+// TODO: implement
+function readFile() {
+	ipcRenderer.send("load_dialog", {
+		do: "load_dialog",
+	});
+	ipcRenderer.on("loaded_file", (event, data) => {
+		console.log("recieved: ");
+		console.log(data);
+		if (!data.canceled) {
+			app_state.load(data.filePaths[0]);
+			app_state.render_todos();
+		}
+	});
+}
